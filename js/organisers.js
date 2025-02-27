97b981d04c4139a0179f91b38b5077526ccad6e8
JAHDavis <101647737+JAHDavis@users.noreply.github.com>
2025-02-27 23:43:50 +0000

    Update organisers.js

--- js/organisers.js
+++ js/organisers.js
@@ -12,7 +12,8 @@ function parseOrganiserCsv(csvText) {
     .map(columns => new Event(...columns));
 }
 
-const organiserCsvData = `Herts SAA,Hertfordshire Schools Athletics Association,The HSAA is responsible for organising county schools championships within Hertfordshire, and also puts on a schools league.`;
+const organiserCsvData = `Herts SAA,Hertfordshire Schools Athletics Association,The HSAA is responsible for organising county schools championships within Hertfordshire, and also puts on a schools league.
+Herts CAAA,Hertfordshire County Amateur Athletic Association,The HCAAA is the county body for athletics within Hertfordshire, organising championships across various disciplines`;
 
 const organisers = parseOrganiserCsv(organiserCsvData);
 
