adb05d95f881fb36dabc5c278616007029b0daba
JAHDavis <101647737+JAHDavis@users.noreply.github.com>
2025-02-28 12:38:27 +0000

    Update officials.js

--- js/officials.js
+++ js/officials.js
@@ -9,12 +9,12 @@ class Official {
 function parseOfficialCsv(csvText) {
   return csvText.split('\n')
     .map(row => row.split(',').map(item => item.trim()))
-    .map(columns => new Event(...columns));
+    .map(columns => new Official(...columns));
 }
 
-const officialCsvData = `1,James,Davis`
+const officialCsvData = `1,James,Davis`;
 
-const officials = parseOfficialCsv(officialCsvData)
+const officials = parseOfficialCsv(officialCsvData);
 
 function selectOfficialById(officialID) {
   return officials.find(official => official.officialID === officialID);
