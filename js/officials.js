f414a48c114911ea3e4bd44d74257a5cfab27d1e
JAHDavis <101647737+JAHDavis@users.noreply.github.com>
2025-02-28 12:36:46 +0000

    Create officials.js

---
+++ js/officials.js
@@ -0,0 +1,21 @@
+class Official {
+  constructor(officialID, firstName, lastName) {
+    this.officialID = officialID;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+function parseOfficialCsv(csvText) {
+  return csvText.split('\n')
+    .map(row => row.split(',').map(item => item.trim()))
+    .map(columns => new Event(...columns));
+}
+
+const officialCsvData = `1,James,Davis`
+
+const officials = parseOfficialCsv(officialCsvData)
+
+function selectOfficialById(officialID) {
+  return officials.find(official => official.officialID === officialID);
+}
